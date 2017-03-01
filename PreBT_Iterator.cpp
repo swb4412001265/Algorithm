d13d84301648800c62c3be7b87b9e66a9e19b029
swb4412001265 <swb4412001265@gmail.com>
2017-03-01 00:52:40 -0500

    PreOrder and PostOrder non-recursive search

--- PreBT_Iterator.cpp
+++ PreBT_Iterator.cpp
@@ -41,3 +41,33 @@ public:
         return result;
     }
 };
+
+
+//////////////////////////////////////////////////////////////
+//Binary Tree Postorder Traversal 
+///////////////////
+class Solution {
+public:
+    vector<int> postorderTraversal(TreeNode *root) {
+        vector<int> result;
+        stack<TreeNode *> myStack;
+        
+        TreeNode *current = root, *lastVisited = NULL;
+        while (current != NULL || !myStack.empty()) {
+            while (current != NULL) {
+                myStack.push(current);
+                current = current->left;
+            }
+            current = myStack.top(); 
+            if (current->right == NULL || current->right == lastVisited) {
+                myStack.pop();
+                result.push_back(current->val);
+                lastVisited = current;
+                current = NULL;
+            } else {
+                current = current->right;
+            }
+        }
+        return result;
+    }
+};
